7eedaa20ad359d0c7f166df530dd3310e3e0bbed
ariesdimasy <ariesdimasy@gmail.com>
2023-07-14 15:35:24 +0700

    update

---
+++ json/index.js
@@ -0,0 +1,18 @@
+const obj = require("./../exercise6")
+
+// let obj = { 
+//     name:"dimas",
+//     age:32
+// }
+
+// const stringify = JSON.stringify(obj)
+// console.log("hello "+stringify+" world")
+// console.log(typeof stringify)
+
+// console.log(JSON.parse(stringify))
+
+// console.log(romanNumeral("XX"))
+// test()
+
+console.log(obj.foo)
+
